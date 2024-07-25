c33c4e33ac2bfa2a61c4de30d706c26bed2f31e0
NoCai <1154266678@qq.com>
2024-07-25 17:20:20 +0800

    nn.Module

---
+++ 6.nn.Module.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+class MyNet(nn.Module):
+    def __init__(self):
+        super(MyNet, self).__init__()
+
+    def forward(self, x):
+        return x+1
+
+net = MyNet()
+x = torch.tensor(1.0)
+print(net(x))
